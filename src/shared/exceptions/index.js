d162c4c74f64d520d769f89f0bede0589ef1ebdb
Aelberth Cheong <aelberth.cheong@outlook.com>
2026-03-01 11:29:31 +0700

    feat: add 401, 403 HTTP error codes

--- src/shared/exceptions/index.js
+++ src/shared/exceptions/index.js
@@ -4,6 +4,14 @@ export default class ClientError extends Error {
         this.statusCode = statusCode;
     }
 
+    static BadRequest(message = "Unauthorized") {
+        return new ClientError(message, 401);
+    }
+
+    static Forbidden(message = "Forbidden") {
+        return new ClientError(message, 403);
+    }
+
     static notFound(message = "Not Found") {
         return new ClientError(message, 404);
     }
