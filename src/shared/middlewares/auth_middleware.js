e83efd80daa67553ab58465cc2518e0d96f29536
Aelberth Cheong <aelberth.cheong@outlook.com>
2026-03-04 09:55:21 +0700

    feat: add auth middleware

---
+++ src/shared/middlewares/auth_middleware.js
@@ -0,0 +1,13 @@
+import ClientError from "../exceptions/client-error";
+import * as TokenManager from "../security/token_manager.js";
+
+export default function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw ClientError.unauthorized("missing token");
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = TokenManager.verifyAccessToken(token);
+    req.user = decoded; // { sub: userId }
+    next();
+}
